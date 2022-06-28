b410ffbbd41ffc08ed77ca3e5fcede9c45df3ca6
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-06-28 20:43:41 +0700

    Web Company Profile - Tesla

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    extend: {
+      // custom warna dan ukuran layar di sini
+    },
+  },
+  plugins: [],
+}
