2a4eefe452a6b88b257cef4f400bdb40b56b1126
Ma'mur Zaky Nurrokhman <mamurzakynurrokhman@gmail.com>
2022-07-05 22:15:04 +0700

    Memperbaiki fungsi tombol Detail Fitur, pop up detail fitur, mengubah font menjadi Work Sans, menambahkan efek background blur saat tombol Detail Fitur diklik

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,15 +1,5 @@
-// Navbar fixed
-// $(document).ready(function () {
-//     window.onscroll = function () {
-//         if (window.pageYOffset > $('header').offset(top)) {
-//             $('header').addClass('navbar-fixed');
-//         } else {
-//             $('header').removeClass('navbar-fixed');
-//         }
-//     }
-// });
+// Menambahkan efek blur pada header ketika web discroll
 window.onscroll = function () {
-    // const header = document.querySelector('header').offsetTop;
     if (window.pageYOffset > document.querySelector('header').offsetTop) {
         $('header').addClass('navbar-fixed');
     } else {
@@ -24,19 +14,21 @@ $('#hamburger').click(function () {
 });
 
 // Klik di luar hamburger
-// const hamburger = document.getElementById('hamburger');
-// const navMenu = document.getElementById('nav-menu');
-
-// window.addEventListener('click', function (e) {
-//     if (e.target != hamburger && e.target != navMenu) {
-//         hamburger.classList.remove('hamburger-active');
-//         navMenu.classList.add('hidden');
-//     }
-// });
-
-$(window).click(function (e) { 
+$(window).click(function (e) {
     if (e.target != document.getElementById('hamburger') && e.target != document.querySelector('nav')) {
         $('#hamburger').removeClass('hamburger-active');
         $('nav').addClass('hidden');
     }
+    if (e.target == document.getElementById('blur-container')) {
+        $('#pop-up').addClass('hidden');
+        $('#blur-container').addClass('hidden');
+    }
+});
+
+// Mengubah gambar saat tombol produk diklik
+
+// Menampilkan pop up saat tombol detail fitur diklik
+$('#detail-fitur').click(function (e) {
+    $('#pop-up').removeClass('hidden');
+    $('#blur-container').removeClass('hidden');
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
   theme: {
     extend: {
       // custom warna dan ukuran layar di sini
+      colors: {
+        'primary-black': '#252525',
+        'primary-white': '#F0F0F0',
+      },
+      borderWidth: {
+        '6': '6px'
+      },
       screens: {
+        'maxmd': {'max': '1023px'},
         'fhd': { 'max': '1080px' },
         'hd': { 'max': '720px' },
         'xs': { 'max': '480px' },
